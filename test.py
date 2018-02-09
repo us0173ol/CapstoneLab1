3bceb4d84f4b5a1a0e348eaa6b8525656cb40830
miked <whatthafunk3@aol.com>
2018-02-09 00:17:09 -0600

    cant get tests to run

--- test.py
+++ test.py
@@ -6,6 +6,6 @@ class TestFunctions(TestCase):
 
 
     def camelCase(self):
-        self.assertEqual("hello", camelCase.camel_Case('HELLO'))
-        self.assertEqual("MIke was hEre", camelCase.camel_Case('mikeWasHere'))
-        self.assertEqual("Unit Testing is fuN", camelCase.camel_Case('unitTestingIsFun'))
+        self.assertEqual("hello", camel_Case('HELLO'))
+        self.assertEqual("MIke was hEre", camel_Case('mikeWasHere'))
+        self.assertEqual("Unit Testing is fuN", camel_Case('unitTestingIsFun'))
